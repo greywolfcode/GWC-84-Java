19b2a447b09c6e9633096760cdfe3f65ecdada24
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-03-26 15:42:24 -0400

    Allow changing of the Default Save Path

---
+++ Menus/SaveManageMenu.java
@@ -0,0 +1,125 @@
+package Menus;
+
+import java.util.Stack;
+
+import GWC_84_Java.Data;
+import GWC_84_Java.Message;
+import GWC_84_Java.Exceptions.NoReturnException;
+
+import ConsoleControl.Colour;
+
+public class SaveManageMenu extends Menu
+{
+    //path/save data
+    private boolean defaultPath = false;
+    private int currentSave = 0;
+    private boolean loadedSave = false; //used for disabling load button
+    private String path;
+    
+    //ui data
+    private int cursorPos = 0;
+    
+    public SaveManageMenu(Data storage, Stack<String> events)
+    {
+        data = storage;
+        setMenuType("action");
+        setGlobalEvents(events);
+    }
+    public void onLoad(String cursor)
+    {
+        cursorPos = 0;
+        //get current save from data
+        Message save;
+        try
+        {
+           save = data.getReturnMessage();
+        }
+        catch(NoReturnException e)
+        {
+            save = new Message("" + data.getDefaultSave());
+        }
+        
+        int saveNum = Integer.parseInt(save.getMString());
+        currentSave = saveNum;
+        
+        if (saveNum == 0)
+        {
+            defaultPath = true;
+            path = data.getDefaultPath();
+            return;
+        }
+        defaultPath = false;
+        path = data.getPath(saveNum);
+        
+        updateScreen();
+    }
+    public void onUnload(){}
+    
+    public void eventHandeler(String state, String event, String cursor)
+    {
+        if (defaultPath)
+        {
+            eventHandelerDefaultPath(event);
+        }
+        else
+        {
+            eventHandelerSave(event);
+        }
+    }
+    
+    private void eventHandelerDefaultPath(String event)
+    {
+        switch(event)
+        {
+            case "w":
+                cursorPos = 0;
+                break;
+            case "s":
+                cursorPos = 1;
+                break;
+            case "clr":
+                pushEvent("switch prevMenu");
+                break;
+            case "ent":
+                if (cursorPos == 1)
+                {
+                    pushEvent("switch MemManageMenu");
+                }
+                break;
+            default:
+                if (cursorPos == 0)
+                {
+                    path = event;
+                    data.setDefaultPath(event);
+                }
+                break;
+        }
+        updateScreen();
+    }
+    private void eventHandelerSave(String event)
+    {
+        
+    }
+    
+    private void updateScreen()
+    {
+        if (defaultPath)
+        {
+            screen[0] = "Default Path:";
+            if (cursorPos == 0)
+            {
+                screen[1] = Colour.invert(path);
+                screen[2] = "Done";
+            }
+            else
+            {
+                screen[1] = path;
+                screen[2] = Colour.invert("Done");
+            }
+        }
+        else
+        {
+            
+        }
+    }
+}
